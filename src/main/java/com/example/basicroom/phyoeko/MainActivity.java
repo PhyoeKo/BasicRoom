c82fde9dc2b8c6cd953463f26bd6564c54898142
Phyoe Ko <developer.skylark@skylarkmyanmar.com>
2019-06-03 15:04:39 +0630

    First Branch

--- src/main/java/com/example/basicroom/phyoeko/MainActivity.java
+++ src/main/java/com/example/basicroom/phyoeko/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.app.AppCompatDelegate;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -38,12 +39,11 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
         recyclerView  =findViewById(R.id.recycler_view);
         et_firstName = findViewById(R.id.et_firstName);
         et_LastName = findViewById(R.id.et_lastName);
         et_phoneNumber = findViewById(R.id.et_phoneNumber);
-
+        getDelegate().setLocalNightMode(AppCompatDelegate.MODE_NIGHT_YES);
         database = Room.databaseBuilder(this, AppDatabase.class, "db-contacts")
                 .allowMainThreadQueries()   //Allows room to do operation on main thread
                 .build();
