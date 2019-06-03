b6c07a6687ff611e7750ae0cadaafecdced7ea2a
Phyoe Ko <developer.skylark@skylarkmyanmar.com>
2019-06-03 12:02:58 +0630

    First Branch

--- src/main/java/com/example/basicroom/phyoeko/AppDatabase.java
+++ src/main/java/com/example/basicroom/phyoeko/AppDatabase.java
@@ -10,4 +10,5 @@ import com.example.basicroom.phyoeko.model.Contact;
 @Database(entities = {Contact.class}, version = 1)
 public abstract class AppDatabase extends RoomDatabase {
     public abstract ContactDAO getContactDAO();
+    //first branch
 }
